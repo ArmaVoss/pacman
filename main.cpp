92148aa501a0b4eb1ea29601fcf7cacb825cf4c7
ArmaVoss <aj.vossoughi@gmail.com>
2025-02-21 13:37:32 -0500

    setting up basic sdl things

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <SDL3/SDL.h>
 
-int main() {
-	SDL_Log("%s", "hello world!");
+int main(int argc, char *argv[]) {
+	bool running{true};
+
+	//create window 
+	SDL_Window *window = SDL_CreateWindow("Pacman", 448, 528, SDL_WINDOW_RESIZABLE);
+	
+	//initialize SDL3
+	SDL_Init(SDL_INIT_VIDEO);
+
+	//check for window creation error
+	if (window == NULL){
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: $s\n", SDL_GetError());
+		return 1;
+	}	
+	
+	//game loop
+	while (running){
+		SDL_Event event;
+		
+		//event loop
+		while(SDL_PollEvent(&event)){
+			if (event.type == SDL_EVENT_QUIT){
+				running = false;
+			}
+		}
+	}
+
+	//close the window and clean up
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
